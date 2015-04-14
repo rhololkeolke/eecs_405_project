f0faa975fa1a64eafd7e1a2588f5ce653ebc075f
Devin Schwab <digidevin@gmail.com>
2015-04-14 16:38:59 -0400

    Now printing only the names. Also using the latin1 decoder to read the file.

--- src/main/java/com/devinschwab/eecs405/util/ImportImdbDatasets.java
+++ src/main/java/com/devinschwab/eecs405/util/ImportImdbDatasets.java
@@ -4,9 +4,16 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ImportImdbDatasets {
+
+    static final Pattern dataStartPattern = Pattern.compile("^Name[\\s]+Titles[\\s]*$");
+    static final Pattern namePattern = Pattern.compile("^(.*,.*)\\t+");
 
     static class CommandLineArgs {
         @Parameter(names = "-in",
@@ -33,7 +40,40 @@ public class ImportImdbDatasets {
             System.exit(1);
         }
 
-        System.out.println(args.imdbDataFile.toString());
-        System.out.println(args.dbName.toString());
+        if (!args.imdbDataFile.isFile()) {
+            throw new IllegalArgumentException(args.imdbDataFile.toString() + " is not a file.");
+        }
+
+        CharsetDecoder latin1Decoder=Charset.forName("latin1").newDecoder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(args.imdbDataFile), latin1Decoder)))
+        {
+
+            String currLine;
+            while ((currLine = br.readLine()) != null) {
+                Matcher lineMatch = dataStartPattern.matcher(currLine);
+                if (lineMatch.matches()) {
+                    break;
+                }
+            }
+
+            // skip the next line
+            // it is just a horizontal break
+            if (br.readLine() == null) {
+                System.err.println("Expected separator line following data column names.");
+                System.exit(2);
+            }
+
+            PrintStream ps = new PrintStream(System.out, true, "UTF8");
+            while((currLine = br.readLine()) != null) {
+                Matcher nameMatch = namePattern.matcher(currLine);
+                if(nameMatch.find()) {
+                    ps.println(nameMatch.group(1));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
